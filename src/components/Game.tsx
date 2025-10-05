d8b429a219ae379663956eec7e62e438d1cec1ef
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-10-05 15:34:45 +0000

    Update Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,10 @@
-import { Fragment, useEffect, useRef, useState } from "react"
+import { Fragment, useReducer, useRef, useState } from "react"
 import { motion, useAnimation, type Transition } from 'framer-motion'
 import type { Team } from "./types"
 import { Button } from "./shadcn/ui/button"
 import animals from '@/words/animals.json'
 import { DateTime } from 'luxon'
+import CountDownClock from "./CountDownClock"
 
 const transition: Transition = {
     duration: 1,
@@ -13,20 +14,14 @@ const transition: Transition = {
 function Game({ teams, setTeams, eachTurnDurationSeconds = 5, finish }: { teams: Team[], setTeams: React.Dispatch<React.SetStateAction<Team[]>>, eachTurnDurationSeconds?: number, finish: () => void }) {
     let controls: any[] = []
 
-    const [playing, setPlaying] = useState(false)
     const [words, _setWords] = useState(animals)
 
     const startedAt = useRef(0)
     const turns = useRef(0)
 
     let angle = 360 / (teams.length * 2) // Since each team has two members
-    teams.map((_, i) => {
-        // let diffRadians = (270 + (angle * i)) / 180
-        // console.log(i, `angle is: ${angle}, angle in radians: ${diffRadians} or ${(180 * diffRadians)}, cos: ${Math.cos(diffRadians)}, sin: ${Math.sin(diffRadians)}`)
-        controls[i] = [useAnimation(), useAnimation()]
-    })
+    teams.map((_, i) => { controls[i] = [useAnimation(), useAnimation()] })
 
-    const firstTick = useRef(true)
     const timer = useRef<number | undefined>(undefined)
 
     const startTimer = (callback: (() => Promise<void>) | (() => void)) => {
@@ -36,21 +31,15 @@ function Game({ teams, setTeams, eachTurnDurationSeconds = 5, finish }: { teams:
         startedAt.current = DateTime.utc().toUnixInteger()
 
         timer.current = setInterval(async () => {
-            if (firstTick.current)
-                firstTick.current = false
-            else {
-                if (DateTime.utc().toUnixInteger() - startedAt.current < 1)
-                    throw new Error('too short tick period')
-
-                await callback()
-            }
+            await callback()
         }, eachTurnDurationSeconds * 1000)
     }
 
     const stopTimer = () => {
+        if (timer.current === undefined)
+            return
         clearInterval(timer.current)
         timer.current = undefined
-        firstTick.current = true
     }
 
     const rotate = () => {
@@ -68,39 +57,44 @@ function Game({ teams, setTeams, eachTurnDurationSeconds = 5, finish }: { teams:
         })
     }
 
-    useEffect(() => {
-        console.log(`playing state has changed: ${playing}`)
+    const initialState = { playing: false };
 
-        if (playing === true) {
-            if (timer.current !== undefined)
+    function reducer(_state: any, action: any) {
+        switch (action.type) {
+            case 'start':
+                if (timer.current !== undefined)
+                    stopTimer()
+
+                startTimer(() => {
+                    console.log('tick')
+                    dispatch({ type: 'timeout' })
+                })
+
+                return { playing: true };
+            case 'timeout':
                 stopTimer()
+                return { playing: false };
+            case 'wordClicked':
+                startTimer(() => {
+                    console.log('tick')
+                    dispatch({ type: 'timeout' })
+                })
 
-            startTimer(() => {
-                console.log('tick')
-                setPlaying(false)
-            })
-        } else
-            stopTimer()
-    }, [playing])
+                return { playing: true };
+            default:
+                throw new Error('Invalid operation dispatched.')
+        }
+    }
 
-    console.log('Game', { firstTick: firstTick.current, timer: timer.current, startedAt: startedAt.current, playing, turns: turns.current })
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    // console.log('Game', { timer: timer.current, startedAt: startedAt.current, state, turns: turns.current })
 
     return (
         <div className="w-full h-full p-2">
-            <Button onClick={() => {
-                turns.current += 1
+            <Button className="my-2" onClick={() => finish()}>End</Button>
 
-                controls.map((cs, i) => {
-                    cs[0].start({
-                        offsetDistance: `${(((i * angle) + 90 + (turns.current * angle)) / 360) * 100}%`,
-                        transition
-                    })
-                    cs[1].start({
-                        offsetDistance: `${(((i * angle) + 270 + (turns.current * angle)) / 360) * 100}%`,
-                        transition
-                    })
-                })
-            }}>Start</Button>
+            {state.playing && <CountDownClock durationSeconds={eachTurnDurationSeconds} resetRef={turns.current} />}
 
             {teams.map((t, i) =>
                 <Fragment key={i}>
@@ -125,36 +119,38 @@ function Game({ teams, setTeams, eachTurnDurationSeconds = 5, finish }: { teams:
                 </Fragment>
             )}
 
-            {!playing &&
+            {!state.playing &&
                 <Button className="top-1/2 left-1/2 absolute -translate-x-[50%] -translate-y-[50%]" onClick={() => {
                     // To do: fetch words
-                    setPlaying(true)
+                    dispatch({ type: 'start' })
                 }}>
                     Start
                 </Button>
             }
 
-            {playing &&
-                <div className="top-1/2 left-1/2 absolute -translate-x-[50%] -translate-y-[50%]" onClick={() => {
-                    stopTimer()
+            {state.playing &&
+                <div
+                    className="top-1/2 left-1/2 absolute -translate-x-[50%] -translate-y-[50%] rounded-full w-[25%] h-[25%]"
+                    onClick={() => {
+                        stopTimer()
 
-                    // Add score
-                    teams[turns.current % teams.length].score += DateTime.utc().toUnixInteger() - startedAt.current
-                    setTeams([...teams])
+                        rotate()
 
-                    rotate()
+                        // Add score
+                        teams[turns.current % teams.length].score += DateTime.utc().toUnixInteger() - startedAt.current
+                        setTeams([...teams])
 
-                    startedAt.current = DateTime.utc().toUnixInteger()
-                    startTimer(() => {
-                        console.log('tick')
-                        setPlaying(false)
-                    })
-                }}>
-                    {words[turns.current % words.length]}
+                        startTimer(() => {
+                            console.log('tick')
+                            dispatch({ type: 'timeout' })
+                        })
+                    }}
+                >
+                    <div className="top-1/2 left-1/2 absolute -translate-x-[50%] -translate-y-[50%] pointer-events-none cursor-pointer">
+                        {words[turns.current % words.length]}
+                    </div>
                 </div>
             }
-
-            <Button onClick={() => finish()}>End</Button>
         </div>
     )
 }
